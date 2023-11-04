f4a0003aaa45eeb91433ef80e795c3ead33a008a
tarunvyas <vyast5529@gmail.com>
2023-11-04 02:46:06 -0700

    first commit

---
+++ fun.js
@@ -0,0 +1,20 @@
+let b1= document.querySelector("#heartb");
+
+b1.addEventListener("click",function()
+{
+alert("registration conformed");
+});
+let i = document.querySelector("#hb");
+i.addEventListener("mouseenter",function(){
+    i.style.color="#e84393"
+;})
+    
+    
+
+
+
+
+    
+    
+
+    
